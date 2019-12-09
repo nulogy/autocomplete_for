ae3407899f6aace71b7fa047ef5c8d94b89bebbd
Alistair McKinnell <alistairm@nulogy.com>
2019-12-09 12:16:46 -0500

    Avoid deprecation warning

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -3,5 +3,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Use integers for SQLite3 databases.
+Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+
 # Initialize the Rails application.
 Rails.application.initialize!
